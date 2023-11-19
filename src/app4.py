f65a9260ebee8f9de75aa9f44c3db62e9e20c768
selenajoetwilliams <selenajoetwilliams@gmail.com>
2023-11-19 13:07:36 -0800

    clearer, cleaner more explicit system in app5.py

--- src/app4.py
+++ src/app4.py
@@ -18,8 +18,6 @@ from api_keys.OPENAI_API_KEY import API_KEY
 app = Flask(__name__)
 openai.api_key = API_KEY
 
-
-# back end
 # initializes LLM & initializes messages list which is a key piece of state / variable throughout the system
 def init_system():
     messages = [] 
@@ -41,6 +39,7 @@ def ask_check_in_prompt(messages: list):
 
     print(f"check in prompt: {check_in_prompt}")
 
+    record("LLM", check_in_prompt)
     messages.append(msg_record)
 
     # write the check in prompt to a txt file for Kuri
@@ -49,6 +48,7 @@ def ask_check_in_prompt(messages: list):
         file.write(check_in_prompt)
 
     return messages
+
 
 def end_session(user_input: str):
     if "end session" in user_input.lower():
@@ -70,26 +70,12 @@ def respond_to_user(messages: list):
     print(f"LLM response: {LLM_response}")
 
     LLM_record = {"role":"assistant", "content": LLM_response}
+
+    record("LLM", LLM_response)
     messages.append(LLM_record)
 
     num_tokens = completion.usage["total_tokens"]
 
-
-# saves conversation to a nicely formatted file
-def save_conversation(messages: list):
-    conversation = ""
-    for msg in messages:
-        if msg["role"] is "system":
-            pass
-        role = msg['role']
-        content = msg['content']
-        formatted_data = f"{role.upper()}: \n{content}\n\n"
-        conversation += formatted_data
-
-    with open('conversation.txt', 'w') as file:
-        file.write(conversation)
-
-    
 
 def get_user_input():
     with open('user_input.txt', 'r') as file:
@@ -97,13 +83,22 @@ def get_user_input():
     
     return user_input
 
+def record(role: str, data: str):
+    with open("conversation.txt","a") as file:
+        formatted_data = f"{role.upper()}: \n {data}\n\n"
+        file.write(formatted_data)
+
+
+
 if __name__ == "__main__":
     print("running appSimply.py ... ")
 
     messages = init_system()
 
     # in check_in_prompt we want to save it as a txt file and send to Kuri to ask via ROS node
-    messages = ask_check_in_prompt(messages) 
+    ask_check_in_prompt(messages) # pass in messages so that messages can be appended
+
+    print(f"messages is: {messages}")
 
     print("having conversation ...")
 
@@ -114,17 +109,16 @@ if __name__ == "__main__":
 
     while not end_session(user_input):
 
-        user_input = get_user_input()
+        user_input = get_user_input() # continually read from file
 
     # if user input is new & non-empty, process it
         if user_input != prev_user_input and user_input != "":
             counter += 1 
-            print(f"while-if counter: {counter}")
             print(f"user input: {user_input}")
 
-            input_record = {"role": "user", "content": user_input}
-
-            messages.append(input_record)
+            record("USER", user_input)
+            formatted_input = {"role": "user", "content": user_input}
+            messages.append(formatted_input)
 
             respond_to_user(messages)
 
@@ -132,24 +126,4 @@ if __name__ == "__main__":
 
     print(f"final messages is \n\n\n{messages}")
     print("WHILE LOOP HAS ENDED!!!!!!")
-    save_conversation(messages)
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def tokens_counter(num_tokens): # this is wrong, restart another time
-#     prev_tokens += num_tokens
-#     print(f'tokens used is now {num_tokens}')
-#     return prev_tokens
-
---
+++ src/app5.py
@@ -0,0 +1,113 @@
+# imports
+import os
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+# speech recongition related imports, will move this to a seperate file soon
+import pyaudio
+import speech_recognition as sr
+import wave
+import random
+import os.path
+import sys
+sys.path.append("..")
+sys.path.append("secrets")
+from api_keys.OPENAI_API_KEY import API_KEY
+app = Flask(__name__)
+openai.api_key = API_KEY
+
+def init_role_content():
+    role_content = """You are a non-diagnosing peer support counselor that 
+    helps people with their mental health. Keep your responses less 
+    than 1 sentence."""
+    return role_content
+
+def formatt_msg(role: str, content: str):
+    return {"role": role, "content": content}
+
+def ask_check_in_prompt():
+    check_in_prompts = ["How are you?", "Have you been stressed lately?"]
+    check_in_prompt = random.choice(check_in_prompts)
+    return check_in_prompt
+
+def end_session(user_input: str):
+    if "end session" in user_input.lower():
+        return True
+    else: 
+        return False
+
+# we only write out the response when we receive the LLM's response, so we
+# always save it to the same file called LLM_response.txt
+def write_out(LLM_response: str):
+    with open('LLM_response.txt', 'w') as file:
+        file.write(LLM_response)
+
+def save_to_conversation_file(role: str, content: str):
+    with open("conversation.txt", "a") as file:
+        formatted_data = f"{role.upper()}: \n {content}\n\n"
+        file.write(formatted_data)
+
+# reads user input file to get user input
+def get_user_response():
+    with open('user_input.txt', 'r') as file:
+        user_input = file.read()
+    
+    return user_input
+
+# takes in messages array with user response, returns LLM response based on
+# conversation thus far
+def get_LLM_response(messages: list):
+    # query the LLM
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+        )
+    
+    # parse the LLM response
+    LLM_response = completion.choices[0].message["content"]
+
+    return LLM_response
+
+if __name__ == "__main__":
+    print("running appSimply.py ... ")
+
+    messages = [] # initialize messages w/ role content & check in prompt
+
+    # get the role content
+    # save to messages array
+    formatted_role_content = formatt_msg(role="system", content=init_role_content)
+    messages.append(formatted_role_content)
+
+    # get the check in prompt 
+    # save to messages array, write to out file, save to conversation
+    check_in_prompt = ask_check_in_prompt()
+    formatted_check_in_prompt = formatt_msg(role="assistant", content=check_in_prompt)
+    messages.append(formatted_check_in_prompt)
+    write_out(check_in_prompt)
+    save_to_conversation_file(role="assistant", content=check_in_prompt)
+    print(f"Role: assistant\n Content: {check_in_prompt}")
+
+    # initialize user input
+    prev_user_input = ""
+
+    # now that user has received check in prompt, await user input
+    user_input = get_user_response()
+
+    while not end_session(user_input):
+        # if the user input is new
+        if user_input != prev_user_input and user_input != "":
+            # save to messages array, add to conversation file
+            formatted_user_input = formatt_msg(role="user", content=user_input)
+            messages.append(formatted_user_input)
+            save_to_conversation_file(role="user", content=user_input)
+            print(f"Role: user\n Content:{user_input}")
+
+            # send user response to LLM, get LLM response
+            LLM_response = get_LLM_response(messages=messages)
+            # write to LLM_response.txt, save to messages array, add to conversation file
+            write_out(LLM_response)
+            formatted_LLM_response = formatt_msg(role="assistant", content=LLM_response)
+            messages.append(formatted_LLM_response)
+            save_to_conversation_file(role="assistant", content=LLM_response)
+            print(f"Role: assistant\n Content: {LLM_response}")
+            
+
