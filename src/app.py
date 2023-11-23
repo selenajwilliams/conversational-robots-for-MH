7ffec3c04fc1ea95839f5496730f673b4dd45a2a
selenajoetwilliams <selenajoetwilliams@gmail.com>
2023-11-22 17:16:28 -0800

    [feat] summarization added

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ from api_keys.OPENAI_API_KEY import API_KEY
 app = Flask(__name__)
 openai.api_key = API_KEY
 
-def init_role_content():
+def get_role_content():
     # role_content = """You are a non-diagnosing peer support counselor that 
     # helps people with their mental health. Keep your responses less 
     # than 1 sentence."""
@@ -48,47 +48,11 @@ def save_to_conversation_file(role: str, content: str):
         file.write(formatted_data)
 
 # reads user input file to get user input
-# def get_user_response():
-#     with open('user_input.txt', 'r') as file:
-#         user_input = file.read()
-    
-#     return user_input
-
-
-# this part below has to do with real time speech recognition & speech to text using the whisper API, 
-# which will enable the get_user_response function above
 def get_user_response():
-    recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
-    print("Listening for speech...")
-
-    with microphone as source:
-        try:
-            print("Start speaking!")
-            recognizer.adjust_for_ambient_noise(source) # optional
-            # save the audio data
-            # params: 
-                # timeout: if no speech is detected within 10 seconds, sr will throw an error or not record
-                # phrase_time_limit: audio stops listening after speech is not detected for 3 seconds 
-                #                    (end of speech detection) ^
-            audio = recognizer.listen(source, timeout=10, phrase_time_limit=3)  # Listen for up to 3 seconds of speech
-            
-            # save audio to file 
-            with open("user_speech.wav", "wb") as audio_file:
-                audio_file.write(audio.get_wav_data())
-
-            # audio_file = open("user_speech.wav", "rb")
-
-            # send audio file to whisper api to be transcribed
-            # transcript = openai.OpenAI.audio.transcriptions.create(model="whisper-1", file=audio_file, response_format="text")
-            
-            transcript = openai.Audio.transcribe("whisper-1", audio_file.read())
-            print(f"\n***SPEECH TRANSCRIPTION***\n{transcript}")
-            return transcript
-        
-        except Exception as e:
-            # if any exception occurs, print it to see what it is
-            print(f"error encountered: {e}")
+    with open('user_input.txt', 'r') as file:
+        user_input = file.read()
+    
+    return user_input
 
 # takes in messages array with user response, returns LLM response based on
 # conversation thus far
@@ -109,7 +73,7 @@ def get_LLM_response(messages: list):
 # returns summarized version
 def summarize(role: str):
     # step 1: parse messages to extract responses based on the role
-    raw_responses = ' '.join(message['content'] for message in messages if message['role'] == role)
+    raw_responses = ' '.join(message['content'] for message in messages_full_log if message['role'] == role)
     print(raw_responses)
 
     # query the api where messages includes instructions to summarize &
@@ -141,20 +105,21 @@ if __name__ == "__main__":
     with open("conversation.txt", "w"):
         pass
 
-    messages = [] # initialize messages w/ role content & check in prompt
-    messages_summary = []
+    messages_full_log = [] # initialize messages w/ role content & check in prompt
+    working_messages = []
 
     # get the role content
     # save to messages array
-    formatted_role_content = format_msg(role="system", content=init_role_content())
-    messages.append(formatted_role_content)
+    formatted_role_content = format_msg(role="system", content=get_role_content())
+    messages_full_log.append(formatted_role_content)
+    working_messages.append(formatted_role_content)
 
     # get the check in prompt 
     # save to messages array, write to out file, save to conversation
     check_in_prompt = ask_check_in_prompt()
     formatted_check_in_prompt = format_msg(role="assistant", content=check_in_prompt)
-    messages.append(formatted_check_in_prompt)
-    messages_summary.append(formatted_check_in_prompt)
+    messages_full_log.append(formatted_check_in_prompt)
+    working_messages.append(formatted_check_in_prompt)
     write_out(check_in_prompt)
     save_to_conversation_file(role="assistant", content=check_in_prompt)
     print(f"Role: assistant\n Content: {check_in_prompt}")
@@ -163,8 +128,7 @@ if __name__ == "__main__":
     user_input = ""
     prev_user_input = ""
     counter = 0
-     
-
+    
     while not end_session(user_input):
 
         # continually get user input
@@ -177,17 +141,19 @@ if __name__ == "__main__":
             counter += 1
             # save to messages array, add to conversation file
             formatted_user_input = format_msg(role="user", content=user_input)
-            messages.append(formatted_user_input)
+            messages_full_log.append(formatted_user_input)
+            working_messages.append(formatted_user_input)
             save_to_conversation_file(role="user", content=user_input)
             print(f"Role: user\n Content:{user_input}")
 
             # send user response to LLM, get LLM response
-            LLM_response = get_LLM_response(messages=messages)
+            LLM_response = get_LLM_response(messages=messages_full_log)
 
             # write to LLM_response.txt, save to messages array, add to conversation file
-            write_out(LLM_response)
+            write_out(LLM_response) # write out LLM response first since we want to get it to user asap
             formatted_LLM_response = format_msg(role="assistant", content=LLM_response)
-            messages.append(formatted_LLM_response)
+            messages_full_log.append(formatted_LLM_response)
+            working_messages.append(formatted_LLM_response)
             save_to_conversation_file(role="assistant", content=LLM_response)
             print(f"Role: assistant\n Content: {LLM_response}")
 
@@ -196,10 +162,21 @@ if __name__ == "__main__":
             if (counter % 2) is 0 and counter is not 0:
                 user_summary = summarize("user")
                 LLM_summary = summarize("assistant")
-                print(f"*** USER SUMMARY: *** \n{user_summary}")
-                print(f"*** LLM SUMMARY: *** \n{LLM_summary}")
+                messages_summary = []
+                formatted_role_content = format_msg(role="system", content=get_role_content())
+                formatted_user_summary = format_msg(role="user", content=user_summary)
+                formatted_LLM_summary = format_msg(role="assistant", content=LLM_summary)
+
+                messages_summary.append(formatted_role_content)
+                messages_summary.append(formatted_user_summary)
+                messages_summary.append(formatted_LLM_summary)
+                
+                working_messages = messages_summary # reset working messages to be the summary 
+
+                # print(f"*** USER SUMMARY: *** \n{user_summary}")
+                # print(f"*** LLM SUMMARY: *** \n{LLM_summary}")
 
 
-            print(f"prev_user_input was: {prev_user_input}")
+            # print(f"prev_user_input was: {prev_user_input}")
             prev_user_input = user_input
-            print(f"prev user input is now: {prev_user_input}")
+            # print(f"prev user input is now: {prev_user_input}")
